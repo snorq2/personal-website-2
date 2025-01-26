8cb30a98b6197c88660ecf2fa4de039b3978a678
snorq2 <sawyernorquist@gmail.com>
2025-01-26 14:59:43 -0800

    Initial footer import

---
+++ src/Components/footer.jsx
@@ -0,0 +1,15 @@
+import '../../styles/styles.css'
+
+function Footer () {
+
+    return (
+        <>
+            <a href="https://www.linkedin.com/in/sawyer-norquist-33040582/">
+                <img src="images/In-White-34.png" alt="LinkedIn" />
+            </a>
+            <p>© Sawyer Norquist 2022</p>
+        </>
+    )
+}
+
+export default Footer
